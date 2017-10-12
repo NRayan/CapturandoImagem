be7c44699bb18a758402f222a5c8d81b27d9d867
Norton Rayan Meira de Oliveira <norton_rayan@hotmail.com>
2017-10-12 05:46:43 -0300

    V 1.0

--- CapturandoImagem/CapturandoImagem/MainPage.xaml.cs
+++ CapturandoImagem/CapturandoImagem/MainPage.xaml.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Plugin.Media;
+using Plugin.Media.Abstractions;
+using System;
+using System.IO;
 using Xamarin.Forms;
 
 namespace CapturandoImagem
@@ -13,5 +12,99 @@ namespace CapturandoImagem
 		{
 			InitializeComponent();
 		}
-	}
+
+        //imagem usada na captura
+        byte[] img = null;
+
+
+        //Conversor Imagem -> Bytes
+        public byte[] ReadFully (Stream Imput)
+        {
+            byte[] buffer = new byte[16 * 1024];
+            using (MemoryStream ms = new MemoryStream())
+            {
+                int read;
+                while((read=Imput.Read(buffer,0,buffer.Length))>0)
+                {
+                    ms.Write(buffer, 0, read);
+                }
+                return ms.ToArray();
+            }
+                
+        }
+
+        private async void btnGaleria_Clicked(object sender, System.EventArgs e)
+        {
+            try
+            {
+                await CrossMedia.Current.Initialize();
+
+                if (!CrossMedia.Current.IsPickPhotoSupported)
+                {
+                    await DisplayAlert("Indísponível", "Recurso indisponível", "Ok");
+                    return;
+                }
+
+                var file = await CrossMedia.Current.PickPhotoAsync(new PickMediaOptions
+                {
+                    PhotoSize = PhotoSize.Small,                    
+                });
+
+                if (file == null)
+                {
+                    return;
+                }
+
+                img = ReadFully(file.GetStream());
+                imgfoto.Source = ImageSource.FromStream(() => file.GetStream());
+            }
+
+            catch(Exception ex)
+            {
+                await this.DisplayAlert("Erro", "Problema ao Executar ação", "ok");
+            }
+        }
+
+        private async void btnCapturar_Clicked(object sender, System.EventArgs e)
+        {
+            try
+            {
+                await CrossMedia.Current.Initialize();
+
+                if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+                {
+                    await DisplayAlert("Sem Camera", "Recurso indisponível", "Ok");
+                    return;
+                }
+
+                var file = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
+                {
+                    PhotoSize = PhotoSize.Full,
+                    AllowCropping=true,
+                    SaveToAlbum = true,
+                    Name = "photo.jpg",
+                    DefaultCamera = CameraDevice.Front,
+                });
+
+                if (file == null)
+                {
+                    return;
+                }
+
+                img = ReadFully(file.GetStream());
+                imgfoto.Source = ImageSource.FromStream(() => file.GetStream());
+            }
+
+            catch (Exception ex)
+            {
+                await this.DisplayAlert("Erro", "Problema ao Executar ação", "ok");
+            }
+        }
+
+        private void btnLimpar_Clicked(object sender, EventArgs e)
+        {
+            img = null;
+            imgfoto.Source = null;
+        }
+    }
 }
